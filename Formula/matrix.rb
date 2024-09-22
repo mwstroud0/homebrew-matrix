487491a9f2258cbfccdf86faa0a47665b8699cc7
mwstroud <mwstroud0@gmail.com>
2024-09-22 18:22:30 -0500

    updated rb

--- Formula/matrix.rb
+++ Formula/matrix.rb
@@ -1,8 +1,8 @@
 class Matrix < Formula
-  desc "A Matrix-like rain effect written in Python"
-  homepage "https://github.com/mwstroud0/matrix-rain"
-  url "https://github.com/mwstroud0/matrix/archive/v1.0.tar.gz"
-  sha256 "your_sha256_hash_here"  # Generate this after creating the tarball
+  desc "A Matrix-like rain effect written in Python with ChatGPT"
+  homepage "https://github.com/mwstroud0/matrix"
+  url "https://github.com/mwstroud0/matrix/releases/download/v1.0/matrix-rain-1.0.tar.gz"
+  sha256 "b9706900eb4a5be4f2a1a20374a1f8e35bc8d8ac0dbe00debd174e429573523a"  # Generate this after creating the tarball
 
   depends_on "python@3.10"
 
