cf9005a361c24afe97cd11cc9090609df050821c
mwstroud <mwstroud0@gmail.com>
2024-09-22 18:47:11 -0500

    cleaned rb

--- Formula/matrix.rb
+++ Formula/matrix.rb
@@ -2,15 +2,14 @@ class Matrix < Formula
   desc "A Matrix-like rain effect written in Python with ChatGPT"
   homepage "https://github.com/mwstroud0/matrix"
   url "https://github.com/mwstroud0/matrix/releases/download/v1.0/matrix-rain-1.0.tar.gz"
-  sha256 "b9706900eb4a5be4f2a1a20374a1f8e35bc8d8ac0dbe00debd174e429573523a"  # Generate this after creating the tarball
+  sha256 "b9706900eb4a5be4f2a1a20374a1f8e35bc8d8ac0dbe00debd174e429573523a"
 
   depends_on "python@3.10"
 
   def install
-    bin.install "matrix"  # The executable launcher
-    prefix.install "matrix.py"  # The main Python script
+    bin.install "matrix"
+    prefix.install "matrix.py"
 
-    # Ensure that the script can find the Python file
     (bin/"matrix").write <<~EOS
       #!/bin/bash
       python3 #{prefix}/matrix.py
